6e99864871a9669b535ed3f1c36bc0fbc34c0a78
josh <github@junk.josh.com>
2021-05-25 15:09:12 -0400

    Node servicing static files and websockets!

--- node-server/index.js
+++ node-server/index.js
@@ -1,15 +1,38 @@
-const WebSocket = require('ws');
+const port = 80;  // Port to listen for connections on
+const express = require('express');     // Webserver
+const app = express();
 
-const port = 80;
-const wss = new WebSocket.Server( { port: port } );
+// Next set up ws to handle the WebSockets
+// https://www.npmjs.com/package/express-ws
+// Seems like we must do this first before adding other `use`s.
+const ws = require('express-ws') (app);
 
-wss.on('connection', function connection(ws) {
-    ws.on('message', function incoming(data) {
-        console.log( "got:" + data );
-        wss.clients.forEach(function each(client) {
-            if (client.readyState === WebSocket.OPEN) {
-                client.send(data);
-            }
-        })
-    })
-})
+app.use(express.static( 'express_static'));          // Serve static files from folder. https://expressjs.com/en/starter/static-files.html
+
+app.ws('/', function(ws, req) {
+    ws.on('message', function(msg) {
+        console.log("msg:"+msg);
+    });
+    ws.on('open', function() {
+        console.log("open");
+    });
+});
+
+const express_server = app.listen( {port: port} );
+
+
+// Connect ws to handle the websocket upgrade event
+
+
+
+// wss.on('connection', function connection(ws) {
+//     ws.on('message', function incoming(data) {
+//         console.log( "got:" + data );
+//         wss.clients.forEach(function each(client) {
+//             if (client.readyState === ws.OPEN) {
+//                 client.send(data);
+//             }
+//         })
+//     })
+// })
+
