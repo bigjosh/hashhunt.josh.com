091a1f7d7d1230d9d67f3940bbfc96791b8c520c
josh <github@junk.josh.com>
2021-06-05 14:36:57 -0400

    waypoint

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # hashhunt.josh.com
  First one to find the hash wins 5 BTC! 
+
+## Client
+
+[here](/express_static/index.html)
+The html/css/js client that the player uses. Locally generates the keys and gets other info to
+generate a block header from the server.
+
+
+## Server
+
+[here](index.js)
+Node.js websocket server. Responsible for...
+
+* sending live info about the block currently being mined to new clients
+* sending notifications to all clients whenever a new block is mined
+* accepting mined blocks from clients and submitting them to the network
+
+Talks to `bitcoin-core` using command line RPC to submit blocks. 
+Has an internal http server that receives notifications of new blocks from a batch file called from bitcoin-core's `blocknotify`.
--- node-server/index.js
+++ node-server/index.js
@@ -1,36 +1,91 @@
-const websocketPort = 80;  // Port to listen for connections on
-const httpPort = 81;       // Used to let bitcoind contact use on blocknotify events
+const websocketPort = 80;  // Port to listen for websocket connections from clients
+const httpPort = 81;       // Used to let bitcoind `blocknotify` tell us about new blocks
 
 const blockSubmitCommandTemplate = "bitcoin-cli.exe submitblock ${block}";  // Passed to exec to submit a found block
 
-const buffer = require('buffer');
+let lastBlockBuffer = Buffer.alloc(256);    // Keep a prefilled buffer around so we can quickly update and send it. Make it way too big here and know about lengths in the code below.
+let lastBlockTimeSecs =0;                   // Time current round started. Updated in update buffer functions.
+setDefaultLastBlockBuffer(lastBlockBuffer); // Put some dummy data in the buffer until we get an update.
 
-const lastHash = buffer.Buffer.from( "00000000000000000008c3fcd3a46bb1beb39dc8bdbad546d595e0b7d665fd20" , "hex" );
-let nbits = 0x170b3ce9;
-let lastblockTimeSecs = Date.now()/1000;        // Now is UTC in ms.
-let blockHeight = 1976988;
+
+console.log("Starting websocket server on port"+websocketPort+"...");
+const wss = startWebsocketServer( websocketPort );
+console.log("Starting http server on port"+httpPort+"...");
+const httpServer = startHttpServer(httpPort);
 
 // Biolerplate from https://www.npmjs.com/package/ws#simple-server
 
-const WebSocket = require('ws');
+function startWebsocketServer( websocketPort ) {
 
-const wss = new WebSocket.Server({ port:websocketPort });
+    const WebSocket = require('ws');
+    const wss = new WebSocket.Server({ port:websocketPort });
 
-// returns a buffer with the info a new connections needs to start up
-// 32 bytes - previous hash
-// 4 bytes - nbits (difficulty)
-// 2 bytes - including seconds since last block
+    // New websocket connection - send startup info: time of last block, prev hash, target
+    wss.on('connection', function connection(ws) {
 
-function startupinfo() {
-    const nowSecs = Date.now()/1000;
-    const b = buffer.Buffer.alloc( 2 + 4 + 32);
-    b.writeUInt32LE( nowSecs , 0 );
-    b.writeUInt32LE( blockHeight , 4 );
-    lastHash.copy( b , 4+4 );                 // So ugly. Where is my b.writeBuffer() ?
-    b.writeUInt32LE( nbits , 4+4+32 );
-    b.writeUInt16LE(  nowSecs - lastblockTimeSecs , 4+4+32+4 );
-    return b;
+        starupNewWsConnection( ws , lastBlockBuffer );
+
+        ws.on('message', function incoming(message) {
+            // For now the only thing we recieve from the client is a mined block, which we turn around and submit.
+            console.log('received: %s', message);
+            submitblock(message);
+        });
+    });
+
+    return wss;
+
 }
+
+// **** We use this buffer to send updates to the clients over the websocket.
+// We do not always need to update all of the info, so fields are in order so that
+// least often updated is at the end. This way we can send a partial buffer to do partial
+// updates and the client can tell how much to update by the length of the message.
+
+// Just set up an old block so there is something to send to new clients who connect before we
+// get the first block....
+
+function  setDefaultLastBlockBuffer(b) {
+    // Info about the last block we know about. Default to this nice block from 5/26/21 until we get next notify
+    let lastBlockHash       = Buffer.from( "00000000000000000008c3fcd3a46bb1beb39dc8bdbad546d595e0b7d665fd20" , "hex" );
+    let lastBlocknbits      = 0x170b3ce9;
+    let lastblockTimeSecs   = 1622081307;        // Now is UTC in ms.
+    let lastBlockHeight     = 685096;
+    updateLastBlockBufferAdjust(b,lastblockTimeSecs,lastBlockHeight,lastBlockHash,lastBlocknbits);
+}
+
+// Update a new block without difficulty adjustment
+
+function updateLastBlockBuffer( b , nowSecs,  height  , prevHash ) {
+
+    lastBlockTimeSecs = Date.now()/1000;        // Time current round started
+
+    b.writeUInt32LE(nowSecs, 0);
+    b.writeUInt32LE(height, 4);
+    prevHash.copy(b.reverse(), 4 + 4);                 // So ugly. Where is my b.writeBuffer() ? Reverse to put in bitcoin LE format so client can use directly.
+    return b.slice( 4 + 4 + 32 );                      // Return a pre-sized buffer
+}
+
+// Update a new block with difficulty adjustment (every 2016 blocks)
+
+function updateLastBlockBufferAdjust( b , nowSecs, height , prevHash ,  nbits  ) {
+    updateLastBlockBuffer( b , nowSecs , height , prevHash );
+    b.writeUInt32LE( nbits , 4+4+32 );
+    return b.slice( 4 + 4 + 32 + 4);                    // Return a pre-sized buffer
+}
+
+// Uses current time and the timestamp of last block to update the "delay time" field  which is only used
+// on the initial send to a new client connection. This is so the client can estimate how long the current
+// round has been going on for.
+
+function updateLastBlockBufferDelay(b) {
+
+    const nowSecs = Date.now()/1000;    // milliseconds to seconds
+    const delaySecs = Math.min( nowSecs - lastBlockTimeSecs , 0xffff);    // Make sure fits in 16 bits
+
+    b.writeUInt16LE(  delaySecs , 4+4+32+4 );
+    return b.slice( 4 + 4 + 32 + 4 + 2);                    // Return a pre-sized buffer
+}
+
 
 // Submit a block to the local bitcoind using the cli
 
@@ -39,50 +94,90 @@ function submitblock( b ) {
     console.log("Submitting block command:"+commandline);
 }
 
-// New websocket connection - send startup info: time of last block, prev hash, target
-wss.on('connection', function connection(ws) {
-    ws
-    ws.send( startupinfo() );
-    ws.on('message', function incoming(message) {
-        console.log('received: %s', message);
-        submitblock(message);
-    });
-});
-
 // Set up an HTTP server so we can get async notifications and share some
-// diagnostics.
+// diagnostics. Returns
 
-const http = require('http');
-var url = require('url');
+var count=0;
 
-http.createServer(function (req, res) {
-    res.write('Hello World!\r'); //write a response to the client
-    // https://nodejs.org/api/url.html
-    // We must supply a base here or else URL will fail with "INVALID_URL" if request doesn't use host header. So stupid.
-    // https://stackoverflow.com/questions/48196706/new-url-whatwg-url-api
-    const myURL = new URL( req.url , "http://stupid.com");
-    const blockhash=myURL.searchParams.get("blockhash");
-    const diff=myURL.searchParams.get("diff");
-    console.log("diff:"+diff+"\r");
-    res.write('blockhash:'+blockhash+'\r');
-//    res.write('diff:'+diff+'\r');
-//    res.write('target:'+diffToTarget(25046487590083.27) +'\r');
-//    res.write('target:'+diffToTarget(diff) +'\r');
-    res.end();
-}).listen( httpPort );
+function startHttpServer(port) {
+
+    const http = require('http');
+    var url = require('url');
+
+    const httpServer = http.createServer(function (req, res) {
+
+        // https://nodejs.org/en/knowledge/HTTP/clients/how-to-access-query-string-parameters/
+        const pathname = url.parse(req.url,true).pathname;
+        const queryObject = url.parse(req.url,true).query;
+
+        console.log("req.url:"+req.url);
+
+        if (pathname.startsWith("/blocknotify")) {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end('Blocknotify good.');
+
+            const blockhash = new Buffer( queryObject.blockhash ).asReversed();
+            const nbits = queryObject.nbits;
+            const height = queryObject.height;
+
+            console.log("blocknotify blockhash="+blockhash+" nbits=" + nbits.toString(16) + " height="+height);
+            blockNotify(blockhash, nbits, height, lastBlockBuffer);       // Send update to clients
+        }
+
+    });
 
 
+    httpServer.listen(port);
+
+    return httpServer;
+
+}
+
+// Send full initialization info to new WS connection to get them started
+
+function starupNewWsConnection( ws , lastBlockBuffer ) {
+
+    // Compute how long it has been since the last block came in. This helps the client show
+    // the player aprox how long into the current round they are.
+
+    const msg = updateLastBlockBufferDelay( lastBlockBuffer );
+    ws.send( msg );
+
+}
 
 // Whenever bitcoin-core gets a new block, then calls the `blocknotify` batch file, which then makes
 // a `curl` call to us with the new blockhash and difficulty.
 
-// TODO: TO submit a block via CLI https://medium.com/stackfame/how-to-run-shell-script-file-or-command-using-nodejs-b9f2455cb6b7
+// TODO: To submit a block via CLI https://medium.com/stackfame/how-to-run-shell-script-file-or-command-using-nodejs-b9f2455cb6b7
 
-function blockNotify( blockhash , diff ) {
+function blockNotify( height , blockhash , nbits , lastBlockBuffer ) {
 
+    const nowSecs = Date.now()/1000;            // Convert milliseconds to seconds. Assumes local machine has good UTC time.
+
+    // Note that we use height+1 since the height passed here is the height of the just-mined block,
+    // but we want to send to the client the height of the next block they should mine.
+
+    if ( blockheight % 2016 == 1 ) {
+        // Difficulty adjustment block. Note we check ==1 and not ==0 becuase we will not see the adjustment in the nbits
+        // until the first block actually mined after the adjustment.
+        // TODO:  Find a way to get the new difficulty sooner. https://bitcoin.stackexchange.com/q/106055/113175
+
+        const msg=updateLastBlockBufferAdjust( lastBlockBuffer , nowSecs, height+1 , blockheight ,  nbits  );
+
+    } else {
+        // Normal new block, no difficulty adjust
+
+        const msg=updateLastBlockBuffer( lastBlockBuffer , nowSecs, height+1 , blockheight  );
+    }
+
+    let count=0;
+    const starttime = performance.now();
     wss.clients.forEach(function each(client) {
         if (client.readyState === WebSocket.OPEN) {
-            client.send(data);
+            client.send(msg);
+            count++;
         }
     });
+    const endtime = performance.now();
+
 }
